15a1389491531ffc6b80a3c1f8c66aad02891912
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-16 09:40:04 +0530

    implementing factories

---
+++ abstract_factory/src/ColorFactory.java
@@ -0,0 +1,14 @@
+import colour.Color;
+import shape.Shape;
+
+public class ColorFactory extends AbstractFactory{
+    @Override
+    Color getColor(String color) {
+        return null;
+    }
+
+    @Override
+    Shape getShape(String shape) {
+        return null;
+    }
+}
