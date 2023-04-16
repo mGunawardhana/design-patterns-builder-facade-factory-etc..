b04c12e96adb5ce5e9a08ba2562f97cfad44911b
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-16 09:21:39 +0530

    implementing the circle class

--- abstract_factory/src/Main.java
+++ abstract_factory/src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
+
+
+    /* Abstract Factory patterns work around a super-factory which creates
+     * other factories. This factory is also called as factory of factories. This
+     * type of design patterns comes under creational pattern as this pattern
+     * provides one of the best ways to create an object.
+     *
+     * In abstract Factory pattern, an interface is responsible for creating
+     * a factory of related objects without explicitly specifying their classes
+     * Each generated factory can give the objects as per the Factory Pattern */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
